35e9a93973aca9a9af0b4a6f5e8a3f96d978e6c1
takuyaw-w <takuya.jamtb@gmail.com>
2020-10-02 01:08:57 +0900

    list3-15, list3-16

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
 import React, {Component} from 'react';
 import './App.css';
+import Rect from "./Rect";
 
 class App extends Component {
   render() {
     return <div>
       <h1>React</h1>
-      <Rect x="100" y="100" w="100" h="100" c="cyan" />
-      <Rect x="150" y="150" w="150" h="150" c="magenta" />
-      <Rect x="200" y="200" w="200" h="200" c="black" />
+      <Rect x="100" y="100" w="100" h="100" c="cyan" r="50"/>
+      <Rect x="150" y="150" w="150" h="150" c="magenta" r="75"/>
+      <Rect x="200" y="200" w="200" h="200" c="black" r="25"/>
     </div>
-  }
-}
-
-class Rect extends React.Component {
-  x = 0;
-  y = 0;
-  width = 0;
-  height = 0;
-  color = "white";
-  style = {};
-
-  constructor(props) {
-      super();
-      this.x = props.x;
-      this.y = props.y;
-      this.width = props.w;
-      this.height = props.h;
-      this.color = props.c;
-      this.style = {
-          backgroundColor: this.color,
-          position: "absolute",
-          left: this.x + "px",
-          top: this.y + "px",
-          width: this.width + "px",
-          height: this.height + "px"
-      }
-  }
-
-  render() {
-      return <div style={this.style}></div>;
   }
 }
 
