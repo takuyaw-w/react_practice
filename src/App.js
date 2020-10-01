f8cdd3b4c9eecea2b30de5cf119e46f590de3a35
takuyaw-w <takuya.jamtb@gmail.com>
2020-10-02 01:04:40 +0900

    list3-13, list3-14

--- src/App.js
+++ src/App.js
@@ -2,18 +2,43 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  render() {
+    return <div>
+      <h1>React</h1>
+      <Rect x="100" y="100" w="100" h="100" c="cyan" />
+      <Rect x="150" y="150" w="150" h="150" c="magenta" />
+      <Rect x="200" y="200" w="200" h="200" c="black" />
+    </div>
+  }
+}
+
+class Rect extends React.Component {
+  x = 0;
+  y = 0;
+  width = 0;
+  height = 0;
+  color = "white";
+  style = {};
 
   constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
+      super();
+      this.x = props.x;
+      this.y = props.y;
+      this.width = props.w;
+      this.height = props.h;
+      this.color = props.c;
+      this.style = {
+          backgroundColor: this.color,
+          position: "absolute",
+          left: this.x + "px",
+          top: this.y + "px",
+          width: this.width + "px",
+          height: this.height + "px"
+      }
   }
 
   render() {
-    return <div>
-      <h1>{this.title}</h1>
-      <p>{this.message}</p>
-    </div>
+      return <div style={this.style}></div>;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title="App" message="This is App Component!" />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
