8552afe4ea156670274cb2c32c7b5cfef522cb3f
takuyaw-w <takuya.jamtb@gmail.com>
2020-10-02 01:01:54 +0900

    list3-11, list3-12

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+
   render() {
     return <div>
-      <h1>React</h1>
-      <p>This is sample component.</p>
-      <p>これはサンプルのコンポーネントです。</p>
+      <h1>{this.title}</h1>
+      <p>{this.message}</p>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="App" message="This is App Component!" />
   </React.StrictMode>,
   document.getElementById('root')
 );
