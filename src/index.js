4f263383f88decb1b403992c410e0e3888b0ef31
takuyaw-w <takuya.jamtb@gmail.com>
2020-10-03 00:30:04 +0900

    list3-20, list3-21

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App msg="Hello App." />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
