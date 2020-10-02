4ae407b8e79483307f713a611a7a052380c6afbf
takuyaw-w <takuya.jamtb@gmail.com>
2020-10-03 00:35:00 +0900

    list3-22

--- src/App.js
+++ src/App.js
@@ -9,9 +9,17 @@ class App extends Component {
     padding: "5px",
     borderBottom: "2px solid #900"
   }
-  btnStyle = {
+  msgStyle2 = {
     fontSize: "20pt",
-    padding: "0px 10px"
+    color: "white",
+    backgroundColor: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
+  btnStyle = {
+    fontSize: "16pt",
+    padding: "10px"
   }
 
   constructor(props) {
@@ -19,22 +27,27 @@ class App extends Component {
     this.state = {
       counter: 0,
       msg: "count start!",
+      flg: true,
     }
-
     this.doAction = this.doAction.bind(this);
   }
 
   doAction(e) {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: `count: ${state.counter}`
+      msg: state.counter,
+      flg: !state.flg
     }));
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      <p style={this.msgStyle}>{this.state.msg}</p>
+      {this.state.flg ?
+      <p style={this.msgStyle}>count: {this.state.msg}</p>
+      :
+      <p style={this.msgStyle2}>{this.state.msg}です。</p>
+      }
       <button style={this.btnStyle} onClick={this.doAction}>Click</button>
     </div>
   }
