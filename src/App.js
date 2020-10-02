1e1d8a86ad7c4013a42caa7b60b233bd7370c810
takuyaw-w <takuya.jamtb@gmail.com>
2020-10-03 00:54:34 +0900

    list3-23

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  data = [];
+
   msgStyle = {
     fontSize: "24pt",
     color: "#900",
     margin: "20px 0px",
     padding: "5px",
-    borderBottom: "2px solid #900"
   }
-  msgStyle2 = {
-    fontSize: "20pt",
-    color: "white",
-    backgroundColor: "#900",
-    margin: "20px 0px",
-    padding: "5px",
-    borderBottom: "2px solid #900"
+  area = {
+    width: "500px",
+    height: "500px",
+    border: "1px solid blue"
   }
   btnStyle = {
     fontSize: "16pt",
@@ -25,30 +23,40 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
-      msg: "count start!",
-      flg: true,
+      list: this.data
     }
     this.doAction = this.doAction.bind(this);
   }
 
   doAction(e) {
-    this.setState((state) => ({
-      counter: state.counter + 1,
-      msg: state.counter,
-      flg: !state.flg
-    }));
+    let x = e.pageX;
+    let y = e.pageY;
+    this.data.push({x:x, y:y});
+    this.setState({
+      list: this.data
+    });
+  }
+
+  draw(d) {
+    let s = {
+      position: "absolute",
+      left: (d.x - 25) + "px",
+      top: (d.y - 25) + "px",
+      width: "50px",
+      height: "50px",
+      backgroundColor: "#66f3",
+    };
+
+    return <div style={s}></div>;
   }
 
   render() {
     return <div>
       <h1>React</h1>
-      {this.state.flg ?
-      <p style={this.msgStyle}>count: {this.state.msg}</p>
-      :
-      <p style={this.msgStyle2}>{this.state.msg}です。</p>
-      }
-      <button style={this.btnStyle} onClick={this.doAction}>Click</button>
+      <h2 style={this.msgStyle}>show rect.</h2>
+      <div style={this.area} onClick={this.doAction}>
+        {this.data.map((value) => this.draw(value))}
+      </div>
     </div>
   }
 }
