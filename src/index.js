50762dba0216de817a7c0da089c8b08ee93e857d
takuyaw-w <takuya.jamtb@gmail.com>
2020-12-02 03:36:46 +0900

    list4-1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// ステートの値
+let state_value = {
+  counter: 0,
+  message: "COUNTER"
+}
+
+// reducers
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1,
+        message: "INCREMENT"
+      };
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1,
+        message: "DECREMENT"
+      }
+    default:
+      return state;
+  }
+}
+
+// create store
+let store = createStore(counter);
+
+// レンダリング
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
