96e7ef7fd98be4719514a5b41e8b48f195d25716
takuyaw-w <takuya.jamtb@gmail.com>
2020-12-03 12:49:02 +0900

    list4-4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
 import './App.css';
-
-// ステートのマッピング
-function mappingState(state) {
-  return state;
-}
+import Memo from "./memo/Memo";
+import AddForm from "./memo/AddForm";
+import FindForm from "./memo/FindForm";
+import DelForm from "./memo/DelForm";
 
 class App extends Component {
+  td = {
+    width: "250px"
+  }
 
   constructor(props) {
     super(props);
@@ -16,66 +18,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Redux</h1>
-        <Message />
-        <Button />
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td style={this.td}><FindForm /></td>
+              <td style={this.td}><DelForm /></td>
+            </tr>
+          </tbody>
+        </table>
+        <Memo />
       </div>
     )
   }
 }
 
-// ストアのコネクト
-App = connect()(App);
-
-// メッセージ表示のコンポーネント
-class Message extends Component {
-  style = {
-    fontSize: "20pt",
-    padding: "20px 5px"
-  }
-
-  render() {
-    return (
-      <p style={this.style}>
-        {this.props.message}: {this.props.counter}
-      </p>
-    );
-  }
-}
-
-// ストアのコネクト
-Message = connect(mappingState)(Message);
-
-class Button extends Component {
-  style = {
-    fontSize: "16pt",
-    padding: "5px 10px"
-  }
-
-  constructor(props) {
-    super(props);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  // ボタンクリックでディスパッチを実行
-  doAction(e) {
-    if (e.shiftKey) {
-      this.props.dispatch({type: 'DECREMENT'});
-    } else {
-      this.props.dispatch({type: 'INCREMENT'});
-    }
-  }
-
-  render() {
-    return (
-      <button style={this.style} onClick={this.doAction}>
-        click
-      </button>
-    )
-  }
-}
-
-// ストアのコネクト
-Button = connect()(Button);
-
-export default App;
+export default connect()(App);
