e30f28a516246a7e5ec9f276340adb870cdb19f0
takuyaw-w <takuya.jamtb@gmail.com>
2020-12-03 12:42:15 +0900

    list4-3

--- src/index.js
+++ src/index.js
@@ -4,38 +4,12 @@ import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
+import MemoStore from "./memo/Store";
 import * as serviceWorker from './serviceWorker';
-
-// ステートの値
-let state_value = {
-  counter: 0,
-  message: "COUNTER"
-}
-
-// reducers
-function counter(state = state_value, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        counter: state.counter + 1,
-        message: "INCREMENT"
-      };
-    case 'DECREMENT':
-      return {
-        counter: state.counter - 1,
-        message: "DECREMENT"
-      }
-    default:
-      return state;
-  }
-}
-
-// create store
-let store = createStore(counter);
 
 // レンダリング
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={MemoStore}>
     <App />
   </Provider>,
   document.getElementById('root')
