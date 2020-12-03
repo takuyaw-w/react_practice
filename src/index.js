6eb37493c2be005ed5ce6c0587653af87fedaaa5
takuyaw-w <takuya.jamtb@gmail.com>
2020-12-03 15:32:41 +0900

    list4-11

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"
+import { PersistGate } from "redux-persist/integration/react";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -24,19 +27,36 @@ function counter(state = state_value, action) {
       return {
         counter: state.counter - 1,
         message: "DECREMENT"
-      }
+      };
+    case 'RESET':
+      return {
+        counter: 0,
+        message: "RESET"
+      };
     default:
       return state;
   }
 }
 
-// create store
-let store = createStore(counter);
+// Redux Persistの設定
+const persistConfig = {
+  key: "root",
+  storage
+}
+
+// パーシストレデューサーの作成
+const persistedReducer = persistReducer(persistConfig, counter);
+
+// ストア、パーシスターの作成
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
 
 // レンダリング
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<p>loading</p>} persistor={pstore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
