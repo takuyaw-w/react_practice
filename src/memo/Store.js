96cf4952c0abcc8226480a8d513ea983fa5002c4
takuyaw-w <takuya.jamtb@gmail.com>
2020-12-03 13:09:49 +0900

    list4-5

---
+++ src/memo/Store.js
@@ -0,0 +1,103 @@
+import { createStore } from "redux";
+
+const initData = {
+    data: [{
+        message: 'sample data',
+        created: new Date()
+    }],
+    message: 'please type message:',
+    mode: 'default',
+    fdata: []
+};
+
+// レデューサー
+export function memoReducer(state = initData, action) {
+    switch (action.type) {
+        case 'ADD':
+            return addReduce(state, action);
+        case 'DELETE':
+            return deleteReduce(state, action);
+        case 'FIND':
+            return findReduce(state, action);
+        default:
+            return state;
+    }
+}
+
+// レデュースアクション
+
+// メモ追加のレデュース処理
+function addReduce(state, action) {
+    let data = {
+        message: action.message,
+        created: new Date()
+    };
+
+    let newdata = state.data.slice();
+    newdata.unshift(data);
+    return {
+        data: newdata,
+        message: 'Added!',
+        mode: 'default',
+        fdata: []
+    };
+}
+
+// メモ検索のレデュース処理
+function findReduce(state, action) {
+    let f = action.find;
+    let fdata = [];
+    state.data.forEach((value) => {
+        if (value.message.indexOf(f) >= 0) {
+            fdata.push(value);
+        }
+    });
+    
+    return {
+        data: state.data,
+        message: `find ${f}:`,
+        mode: 'find',
+        fdata: fdata
+    };
+}
+
+// メモ削除のレデュース処理
+function deleteReduce(state, action) {
+    let newdata = state.data.slice();
+    newdata.splice(action.index, 1);
+
+    return {
+        data: newdata,
+        message: `delete ${action.index}:`,
+        mode: 'delete',
+        fdata: []
+    }
+}
+
+// アクションクリエーター
+
+// メモ追加のアクション
+export function addMemo(text) {
+    return {
+        type: 'ADD',
+        message: text
+    }
+}
+
+// メモ削除のアクション
+export function deleteMemo(num) {
+    return {
+        type: 'DELETE',
+        index: num
+    }
+}
+
+// メモ検索のアクション
+export function addMemo(text) {
+    return {
+        type: 'FIND',
+        find: text
+    }
+}
+
+export default createStore(memoReducer);
