4318a22011b7fed0f83ca247eb412a93edfd0079
takuyaw-w <takuya.jamtb@gmail.com>
2020-12-04 10:36:56 +0900

    list4-13

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 import './index.css';
 import App from './App';
-import MemoStore from "./memo/Store";
+import MemoStore, { memoReducer } from "./memo/Store";
 import * as serviceWorker from './serviceWorker';
+
+// redux persistの設定
+const persistConfig = {
+  key: "memo",
+  storage
+}
+
+// パーシストレデューサーの作成
+const persistedReducer = persistReducer(persistConfig, memoReducer);
+
+// ストア、パーシスターの作成
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
 
 // レンダリング
 ReactDOM.render(
-  <Provider store={MemoStore}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>} persistor={pstore}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
