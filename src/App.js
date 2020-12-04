ba2c3a6f0e4697c272188423da446c15d04efdb0
takuyaw-w <takuya.jamtb@gmail.com>
2020-12-04 11:33:57 +0900

    list4-20

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Memo from "./memo/Memo";
 import AddForm from "./memo/AddForm";
 import FindForm from "./memo/FindForm";
 import DelForm from "./memo/DelForm";
+import PersistForm from './memo/PersistForm';
 
 class App extends Component {
   td = {
@@ -26,6 +27,7 @@ class App extends Component {
             <tr>
               <td style={this.td}><FindForm /></td>
               <td style={this.td}><DelForm /></td>
+              <td style={this.td}><PersistForm /></td>
             </tr>
           </tbody>
         </table>
